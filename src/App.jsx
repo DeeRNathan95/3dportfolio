bb3079b204663449312f2c4239ef3776d36309b9
Dee <d.r.nathan@outlook.com>
2025-12-05 22:41:11 +0100

    Project section, navbar and experienceCard added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Hero from "./components/sections/Hero.jsx";
+import ShowcaseSection from "./components/sections/ShowcaseSection.jsx";
+import NavBar from "./components/NavBar.jsx";
+import FeatureCards from "./components/sections/FeatureCards.jsx";
+import ExperienceSection from "./components/sections/ExperienceSection.jsx";
 
 const App = () => {
     return (
        <>
+           <NavBar />
        <Hero />
+           <ShowcaseSection />
+           <FeatureCards />
+           <ExperienceSection />
        </>
     )
 }
---
+++ src/components/sections/ExperienceSection.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import TitleHeader from "../HeroModels/TitleHeader.jsx";
+
+const ExperienceSection = () => {
+    return (
+        <section id={'experience'} className={'flex-center md:mt-40 mt-20 section-padding xl:px-0'}>
+            <div className={'className="w-full h-full md:px-20 px-5'}>
+        <TitleHeader
+            title={'Professional Work Experience'}
+            sub={'My Career Overview'} />
+            </div>
+        </section>
+    )
+}
+export default ExperienceSection
